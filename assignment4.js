a4849059ee47e6a8a0a7d737e7764d620890b266
zwang42 <93899615+zwang42@users.noreply.github.com>
2022-05-31 16:42:59 -0700

    logic done, hit detection+levels+score+gameover

--- assignment4.js
+++ assignment4.js
@@ -6,6 +6,56 @@ const {
 
 const {Cube, Axis_Arrows, Textured_Phong} = defs
 
+function edges(x, y) {
+    return [x-1, x+1, y-1, y+1];
+
+}
+export class Text_Line extends Shape {                           // **Text_Line** embeds text in the 3D world, using a crude texture
+                                                                 // method.  This Shape is made of a horizontal arrangement of quads.
+                                                                 // Each is textured over with images of ASCII characters, spelling
+                                                                 // out a string.  Usage:  Instantiate the Shape with the desired
+                                                                 // character line width.  Then assign it a single-line string by calling
+                                                                 // set_string("your string") on it. Draw the shape on a material
+                                                                 // with full ambient weight, and text.png assigned as its texture
+                                                                 // file.  For multi-line strings, repeat this process and draw with
+                                                                 // a different matrix.
+    constructor(max_size) {
+        super("position", "normal", "texture_coord");
+        this.max_size = max_size;
+        var object_transform = Mat4.identity();
+        for (var i = 0; i < max_size; i++) {                                       // Each quad is a separate Square instance:
+            defs.Square.insert_transformed_copy_into(this, [], object_transform);
+            object_transform.post_multiply(Mat4.translation(1.5, 0, 0));
+        }
+    }
+
+    set_string(line, context) {           // set_string():  Call this to overwrite the texture coordinates buffer with new
+        // values per quad, which enclose each of the string's characters.
+        this.arrays.texture_coord = [];
+        for (var i = 0; i < this.max_size; i++) {
+            var row = Math.floor((i < line.length ? line.charCodeAt(i) : ' '.charCodeAt()) / 16),
+                col = Math.floor((i < line.length ? line.charCodeAt(i) : ' '.charCodeAt()) % 16);
+
+            var skip = 3, size = 32, sizefloor = size - skip;
+            var dim = size * 16,
+                left = (col * size + skip) / dim, top = (row * size + skip) / dim,
+                right = (col * size + sizefloor) / dim, bottom = (row * size + sizefloor + 5) / dim;
+
+            this.arrays.texture_coord.push(...Vector.cast([left, 1 - bottom], [right, 1 - bottom],
+                [left, 1 - top], [right, 1 - top]));
+        }
+        if (!this.existing) {
+            this.copy_onto_graphics_card(context);
+            this.existing = true;
+        } else
+            this.copy_onto_graphics_card(context, ["texture_coord"], false);
+    }
+}
+function collision(l1, r1, l2, r2)
+{
+    return ((l1 < l2 && l2 < r1 )||(l1<r2 && r2 < r1))
+}
+
 export class Assignment4 extends Scene {
     /**
      *  **Base_scene** is a Scene that can be added to any display canvas.
@@ -21,7 +71,8 @@ export class Assignment4 extends Scene {
         this.shapes = {
             box_1: new Cube(),
             box_2: new Cube(),
-            axis: new Axis_Arrows()
+            text_1: new Text_Line(35),
+            axis: new Axis_Arrows(),
         }
         console.log(this.shapes.box_1.arrays.texture_coord)
 
@@ -35,10 +86,15 @@ export class Assignment4 extends Scene {
             }),
             texture: new Material(new Textured_Phong(), {
                 color: hex_color("#ffffff"),
-                ambient: 0.5, diffusivity: 0.1, specularity: 0.1,
+                ambient: 1, diffusivity: 0, specularity: 0,
                 texture: new Texture("assets/stars.png")
             }),
+            text_image: new Material(new Textured_Phong(), {
+                ambient: 1, diffusivity: 0, specularity: 0,
+                texture: new Texture("assets/text.png")
+            }),
         }
+
         this.initial_camera_location = Mat4.look_at(vec3(7, 4, 18), vec3(7, 5, 0), vec3(0, 1, 0));
         this.obstacle_locations = [
             {obx: 20, xnoise: Math.random()*2-1,
@@ -56,13 +112,35 @@ export class Assignment4 extends Scene {
         this.velocity = 0.0;
         this.jump = false;
         this.jump_time = 0;
-        this.duck = false;
+        this.gameover = 0;
+        this.reset_flag = 0;
     }
-
+    restart(context, program_state){
+        program_state.animation_time=0;
+        this.initial_camera_location = Mat4.look_at(vec3(7, 4, 18), vec3(7, 5, 0), vec3(0, 1, 0));
+        this.obstacle_locations = [
+            {obx: 20, xnoise: Math.random()*2-1,
+                oby: Math.random()*5, spawn_time: 0, color:color(Math.random(), Math.random(), Math.random(), 1)},
+            {obx: 35, xnoise: Math.random()*2-1,
+                oby: Math.random()*5, spawn_time: 0, color:color(Math.random(), Math.random(), Math.random(), 1)},
+            {obx: 50, xnoise: Math.random()*2-1,
+                oby: Math.random()*5,spawn_time: 0, color:color(Math.random(), Math.random(), Math.random(), 1)},
+            {obx: 65, xnoise: Math.random()*2-1,
+                oby: Math.random()*5,spawn_time: 0, color:color(Math.random(), Math.random(), Math.random(), 1)},
+            {obx: 80, xnoise: Math.random()*2-1,
+                oby: Math.random()*5,spawn_time: 0, color:color(Math.random(), Math.random(), Math.random(), 1)},
+        ]
+        this.player_y = 0.0;
+        this.velocity = 0.0;
+        this.jump = false;
+        this.jump_time = 0;
+        this.gameover = 0;
+    }
     make_control_panel() {
         // TODO:  Implement requirement #5 using a key_triggered_button that responds to the 'c' key.
         this.key_triggered_button("Jump", ["ArrowUp"], ()=>this.jump = true, hex_color('#ff0000'), ()=>this.jump=false);
         this.key_triggered_button("Duck", ["ArrowDown"], ()=>this.duck=true, hex_color('#ff0000'), ()=>this.duck=false);
+        //this.key_triggered_button("Restart", ["q"], ()=>this.reset_flag=1);
 
     }
 
@@ -72,7 +150,6 @@ export class Assignment4 extends Scene {
             // Define the global camera and projection matrices, which are stored in program_state.
             program_state.set_camera(this.initial_camera_location);
         }
-        let alive = 1;
         program_state.projection_transform = Mat4.perspective(
             Math.PI / 4, context.width / context.height, 1, 100);
 
@@ -81,10 +158,14 @@ export class Assignment4 extends Scene {
 
         let t = program_state.animation_time / 1000, dt = program_state.animation_delta_time / 1000;
         let model_transform = Mat4.identity();
-
-        // TODO:  Draw the required boxes. Also update their stored matrices.
-        // You can remove the folloeing line.
-        if(alive){
+        let level =Math.min(Math.floor(t/15+1), 5);
+        // if(this.reset_flag)
+        // {
+        //     this.restart(context, program_state);
+        // }
+        if (!this.gameover) {
+            // TODO:  Draw the required boxes. Also update their stored matrices.
+            // You can remove the folloeing line.
             //player
             let model_transform_player = model_transform;
             if (this.jump && this.player_y == 0){
@@ -104,32 +185,57 @@ export class Assignment4 extends Scene {
                     model_transform_player = model_transform_player.times(Mat4.scale(1,1/2,1)).times(Mat4.translation(0,-1,0));
                 }
             }
+            this.shapes.box_1.draw(context, program_state, model_transform_player, this.materials.phong);
 
-
-            this.shapes.box_1.draw(context, program_state, model_transform_player, this.materials.phong.override({color: hex_color("#ff0000")}));
+            let plocs = edges(model_transform_player[0][3], model_transform_player[1][3])
+            //score
+            let score = t*3
+            score = "Score:" + Math.floor(score).toString();
+            this.shapes.text_1.set_string(score, context.context);
+            this.shapes.text_1.draw(context, program_state, model_transform.times(Mat4.translation(5, 11.5, 0)).times(Mat4.scale(0.3, 0.3, 0.3)), this.materials.text_image);
+            this.shapes.text_1.set_string("Level: "+level, context.context);
+            this.shapes.text_1.draw(context, program_state, model_transform.times(Mat4.translation(16, 11.5, 0)).times(Mat4.scale(0.3, 0.3, 0.3)), this.materials.text_image);
             //obstacles
-            for(let i = 0; i < this.obstacle_locations.length; i++)
-            {
-                let x_loc = this.obstacle_locations[i].obx-12*(t-this.obstacle_locations[i].spawn_time);
-                if (x_loc < -11)
-                {
+
+            for (let i = 0; i < this.obstacle_locations.length; i++) {
+                let x_loc = this.obstacle_locations[i].obx - (10+level*4)* dt;//(t - this.obstacle_locations[i].spawn_time);
+                this.obstacle_locations[i].obx = x_loc;
+                if (x_loc < -11) {
                     this.obstacle_locations[i].obx = 69;
-                    this.obstacle_locations[i].oby = Math.random()*5;
-                    this.obstacle_locations[i].spawn_time = t;
-                    this.obstacle_locations[i].xnoise = Math.random()*2-1;
+                    this.obstacle_locations[i].oby = Math.random() * 5;
+                    //this.obstacle_locations[i].spawn_time = t;
+                    this.obstacle_locations[i].xnoise = Math.random() * 2 - 1;
                     this.obstacle_locations[i].color = color(Math.random(), Math.random(), Math.random(), 1)
                     x_loc = this.obstacle_locations[i].obx;
                 }
-                x_loc+= this.obstacle_locations[i].xnoise;
+                x_loc += this.obstacle_locations[i].xnoise;
                 let model_transform_obstacle = model_transform.times(Mat4.translation(x_loc, this.obstacle_locations[i].oby, 0));
+                let olocs = edges(model_transform_obstacle[0][3], model_transform_obstacle[1][3]);
 
-                this.shapes.box_2.draw(context, program_state, model_transform_obstacle, this.materials.phong.override({color:this.obstacle_locations[i].color}));
+                if (collision(plocs[0], plocs[1], olocs[0], olocs[1]) && collision(plocs[2], plocs[3], olocs[2], olocs[3])) {
+                    this.gameover = 1;
+                    //document.getElementById('game-over-layout').classList.toggle('activate');
+                    break;
+                }
+                this.shapes.box_2.draw(context, program_state, model_transform_obstacle, this.materials.phong.override({color: this.obstacle_locations[i].color}));
             }
+        }
+        else
+        {
+            let print_string = "GAME OVER"
+            this.shapes.text_1.set_string(print_string, context.context);
+            this.shapes.text_1.draw(context, program_state, model_transform.times(Mat4.translation(0, 6, 0))
+                .times(Mat4.scale(1.1, 1.1, 1.1)), this.materials.text_image);
+            this.shapes.text_1.set_string("Refresh to restart", context.context);
+            this.shapes.text_1.draw(context, program_state, model_transform.times(Mat4.translation(2, 3, 0))
+                .times(Mat4.scale(0.4, 0.4, 0.4)), this.materials.text_image);
 
 
 
+            //refresh on click
         }
     }
+
 }
 
 
